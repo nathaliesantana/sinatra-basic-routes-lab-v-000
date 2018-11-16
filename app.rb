a0ea593f298d9391c80a1517a5151c31f7807e08
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-16 05:00:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,21 @@ class App < Sinatra::Base
   get "/" do
     "Hello, World!"
   end
+
+  get "/name" do
+    resp.status: 200
+    "My name is "
+  end
+
+  get "/hometown" do
+    resp.status: 200
+    "My hometown is "
+  end
+
+  get "/favorite-song" do
+    resp.status: 200
+    "My favorite song is  "
+  end
+
+
 end
