611a05a4b375a566f606d6b478573c6d33ec6812
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-16 05:05:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    "Hello, World!"
+  end
+
+  get "/name" do
+    status = 200
+    "My name is "
+  end
+
+  get "/hometown" do
+    status = 200
+    "My hometown is "
+  end
+
+  get "/favorite-song" do
+    status = 200
+    "My favorite song is  "
+  end
+
+
 end
